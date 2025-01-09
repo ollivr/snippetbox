17a2defc9da8e1a5f1178e6964a4da4d22ae7905
Ollivr <66511791+ollivr@users.noreply.github.com>
2025-01-09 01:29:02 -0800

    added go template functions and template pages

---
+++ cmd/web/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
+	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+
+	log.Print("Starting server on port 8080")
+
+	err := http.ListenAndServe(":8080", mux)
+	log.Fatal(err)
+}
