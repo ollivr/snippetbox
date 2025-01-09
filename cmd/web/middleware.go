59da00efe578fb623c724e4208bbd872f63293e9
Ollivr <66511791+ollivr@users.noreply.github.com>
2025-01-09 13:16:03 -0800

    added logger middleware for requests

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -4,6 +4,21 @@ import (
 	"net/http"
 )
 
+func (app *application) logRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var (
+			ip     = r.RemoteAddr
+			proto  = r.Proto
+			method = r.Method
+			uri    = r.URL.RequestURI()
+		)
+
+		app.logger.Info("received request", "ip", ip, "proto", proto, "method", method, "uri", uri)
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func commonHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
